5e606f2b145ec604b3ef09c641ac4a3d1c6995eb
nuita <1397231507@qq.com>
2023-01-05 20:50:33 +0800

    优化代码

--- robot_detection/armor_detection/armor_detecte.cpp
+++ robot_detection/armor_detection/armor_detecte.cpp
@@ -1,23 +1,30 @@
 #include "armor_detecte.h"
 
-namespace armor_detection {
-    armor_detecte::armor_detecte(const InputImg &img_origin){
+namespace armor_detection
+{   
+    const double armor_detecte::_PI_ = 3.141592653;
+
+    armor_detecte::armor_detecte(const InputImg &img_origin)
+    {
         this->img_origin = img_origin;
     }
-    
-    armor_detecte::armor_detecte(){}
 
-    void armor_detecte::setImg_origin(const InputImg &img_origin){
+    armor_detecte::armor_detecte() {}
+
+    void armor_detecte::setImg_origin(const InputImg &img_origin)
+    {
         this->img_origin = img_origin;
     }
 
     void armor_detecte::loadParam(bool enermy_color, bool isParam,
-                    int alpha, int beta, int thresh_value,
-                    double light_bit_min, double light_bit_max, double light_angle_min, double light_angle_max, double light_area_min, double light_area_max, double light_angle_abs,
-                    double armor_bit_min, double armor_bit_max, double armor_area_min, double armor_area_max){
-        
+                                  int alpha, int beta, int thresh_value,
+                                  double light_bit_min, double light_bit_max, double light_angle_min, double light_angle_max, double light_area_min, double light_area_max, double light_angle_abs,
+                                  double armor_bit_min, double armor_bit_max, double armor_area_min, double armor_area_max)
+    {
+
         this->enermy_color = enermy_color;
-        if(isParam == true){
+        if (isParam == true)
+        {
             // init
             this->alpha = alpha;
             this->beta = beta;
@@ -38,41 +45,43 @@ namespace armor_detection {
             this->armor_area_min = armor_area_min;
             this->armor_area_max = armor_area_max;
         }
-        else if(enermy_color == ENERMY_COLOR_BLUE){
-           // init
+        else if (enermy_color == ENERMY_COLOR_BLUE)
+        {
+            // init
             this->alpha = 1, this->beta = 50;
             this->thresh_value = 180;
-            
+
             // light
             this->light_bit_min = 13;
             this->light_bit_max = 30;
             this->light_angle_min = -100;
-            this->light_angle_max = 100; 
+            this->light_angle_max = 100;
             this->light_area_min = 100;
             this->light_area_max = 1500;
             this->light_angle_abs = 10;
 
-            // armor 
+            // armor
             this->armor_bit_min = 30;
             this->armor_bit_max = 60;
             this->armor_area_min = 0;
             this->armor_area_max = 10000;
         }
-        else{
+        else
+        {
             // init
             this->alpha = 1, beta = 50;
             this->thresh_value = 180;
-            
+
             // light
             this->light_bit_min = 13;
             this->light_bit_max = 25;
             this->light_angle_min = -17;
-            this->light_angle_max = 17; 
+            this->light_angle_max = 17;
             this->light_area_min = 100;
             this->light_area_max = 10000;
             this->light_angle_abs = 4;
 
-             // armor 
+            // armor
             this->armor_bit_min = 0.3552;
             this->armor_bit_max = 0.7552;
             this->armor_area_min = armor_area_min;
@@ -80,10 +89,11 @@ namespace armor_detection {
         }
     }
 
-    void armor_detecte::init() {
+    void armor_detecte::init()
+    {
 
         Mat img_gray, img_HSV, img_sub;
-        
+
         vector<Mat> channels;
         img_origin.convertTo(img_change, -1, alpha, beta);
         cvtColor(img_change, img_HSV, COLOR_BGR2HSV);
@@ -94,7 +104,7 @@ namespace armor_detection {
 
         threshold(img_gray, img_thresh, thresh_value, 255, THRESH_BINARY);
 
-        Mat kernal = getStructuringElement(MORPH_RECT, Size(6, 6));  //
+        Mat kernal = getStructuringElement(MORPH_RECT, Size(6, 6)); //
         // 闭运算
         dilate(img_sub, img_dilate, kernal, Point(-1, -1), 1);
         erode(img_dilate, img_erode, kernal, Point(-1, -1), 1);
@@ -107,45 +117,52 @@ namespace armor_detection {
         //  vector<Mat> imgs = {img_change, img_thresh, img_dialte, ans};
     }
 
-    void armor_detecte::contours() {
+    void armor_detecte::contours()
+    {
         RotatedRect rect;
         vector<vector<Point>> points;
         contours_rects.clear();
-        
+
         findContours(img_and, points, RETR_EXTERNAL, CHAIN_APPROX_SIMPLE);
-        for (auto p : points) {
+        for (auto p : points)
+        {
             rect = minAreaRect(p);
             contours_rects.push_back(rect);
         }
     }
 
-    void armor_detecte::contours_to_light() {
+    void armor_detecte::contours_to_light()
+    {
         vector<RotatedRect> rects_select;
         float angle, angle_abs;
         int m = -1, n = 1;
 
         light_rects.clear();
         // 根据长宽比 角度 面积筛选
-        for (int i = 0; i < contours_rects.size(); i++) {
+        for (int i = 0; i < contours_rects.size(); i++)
+        {
             double area = contours_rects[i].size.area();
             double bit = min(contours_rects[i].size.width, contours_rects[i].size.height) /
-                        max(contours_rects[i].size.width, contours_rects[i].size.height);
+                         max(contours_rects[i].size.width, contours_rects[i].size.height);
             double angle = contours_rects[i].angle;
             if (bit * 100 > light_bit_min && bit * 100 < light_bit_max &&
-                    angle > light_angle_min && angle < light_angle_max &&
-                    area > light_area_min && area < light_area_max)
+                angle > light_angle_min && angle < light_angle_max &&
+                area > light_area_min && area < light_area_max)
                 rects_select.push_back(contours_rects[i]);
         }
 
         // 根据角度差筛选
-        for (int i = 0; i < rects_select.size(); i++) {
+        for (int i = 0; i < rects_select.size(); i++)
+        {
             angle = rects_select[i].angle;
             m = -1;
-            for (int j = i + 1; j < rects_select.size(); j++){
+            for (int j = i + 1; j < rects_select.size(); j++)
+            {
                 angle_abs = std::abs(angle - rects_select[j].angle);
-                if (angle_abs < 1e-3)//选到相同的
+                if (angle_abs < 1e-3) // 选到相同的
                     continue;
-                if (angle_abs < light_angle_abs){
+                if (angle_abs < light_angle_abs)
+                {
                     m = j;
                 }
             }
@@ -155,11 +172,13 @@ namespace armor_detection {
         }
     }
 
-    void armor_detecte::light_to_armor() {
+    void armor_detecte::light_to_armor()
+    {
         float point_distance, armor_angle, armor_bit, armor_area, height, point_tan;
 
         armor_rects.clear();
-        for (int i = 0; i < light_rects.size(); i++){
+        for (int i = 0; i < light_rects.size(); i++)
+        {
             for (int j = i + 1; j < light_rects.size(); j++)
             {
 
@@ -173,7 +192,7 @@ namespace armor_detection {
 
                 if (armor_area < armor_area_min || armor_area > armor_area_max)
                     continue;
-                    
+
                 Point2f center(
                     (light_rects[j].center.x + light_rects[i].center.x) / 2,
                     (light_rects[j].center.y + light_rects[i].center.y) / 2);
@@ -183,7 +202,7 @@ namespace armor_detection {
                         (light_rects[j].center.y - light_rects[i].center.y) /
                         (light_rects[j].center.x - light_rects[i].center.x)) /
                     _PI_ * 180;
-                
+
                 // if (armor_angle > 17 || armor_angle < -17)
                 //     continue;
 
@@ -192,23 +211,27 @@ namespace armor_detection {
             }
         }
     }
-    void armor_detecte::drawContours(){
-        for(auto rect:light_rects){
-            Point2f* points = new Point2f[4];
+    void armor_detecte::drawContours()
+    {
+        for (auto rect : light_rects)
+        {
+            Point2f *points = new Point2f[4];
             rect.points(points);
-             for (int i = 0; i < 4; i++)
+            for (int i = 0; i < 4; i++)
                 line(img_origin, points[i], points[(i + 1) % 4], Scalar(0, 0, 255), 2);
-        }            
+        }
     }
 
-    void armor_detecte::drawrotate() {
+    void armor_detecte::drawrotate()
+    {
         int i = 0;
         double area, bit;
         double x, y;
         string str_x, str_y;
 
-        for (auto rect : armor_rects) {
-            Point2f* points = new Point2f[4];
+        for (auto rect : armor_rects)
+        {
+            Point2f *points = new Point2f[4];
             rect.points(points);
             for (int i = 0; i < 4; i++)
                 line(img_origin, points[i], points[(i + 1) % 4], Scalar(0, 0, 255), 2);
@@ -224,21 +247,25 @@ namespace armor_detection {
         }
     }
 
-    void armor_detecte::showImg(){
+    void armor_detecte::showImg()
+    {
         vector<Mat> imgs = {img_change, img_thresh, img_erode, img_and};
         four_to_one(imgs, img_combination);
         imshow("img_combination", img_combination);
         imshow("img", img_origin);
     }
 
-    void armor_detecte::four_to_one(const vector<cv::Mat>& imgs, OutputImg& ans) {
+    void armor_detecte::four_to_one(const vector<cv::Mat> &imgs, OutputImg &ans)
+    {
         int height = imgs[0].rows;
         int width = imgs[0].cols;
         cv::Mat img_combination_;
         vector<Mat> img_channels;
         cv::Mat img = Mat::zeros(Size(width * 2, height * 2), imgs[0].type());
-        for (int i = 0, j = 0, k = 0; i < 4; i++) {
-            if (imgs[i].channels() == 1) {
+        for (int i = 0, j = 0, k = 0; i < 4; i++)
+        {
+            if (imgs[i].channels() == 1)
+            {
                 img_channels.clear();
                 img_channels.push_back(imgs[i]);
                 img_channels.push_back(imgs[i]);
@@ -247,7 +274,8 @@ namespace armor_detection {
                 img_combination_.copyTo(
                     img(Rect(width * j, height * k, width, height)));
                 img_combination_.release();
-            } else
+            }
+            else
                 imgs[i].copyTo(img(Rect(width * j, height * k, width, height)));
             if (i != 1)
                 j++;
@@ -256,4 +284,4 @@ namespace armor_detection {
         }
         img.copyTo(ans);
     }
-}  // namespace armor_detection
+} // namespace armor_detection
--- robot_detection/armor_detection/armor_detecte.h
+++ robot_detection/armor_detection/armor_detecte.h
@@ -6,15 +6,16 @@
 #include <opencv2\opencv.hpp>
 #include <cmath>
 
-#define _PI_ 3.141592653
 #define InputImg cv::Mat
 #define OutputImg cv::Mat
 
 using namespace cv;
 using namespace std;
 
-namespace armor_detection{
-    class armor_detecte{
+namespace armor_detection
+{
+    class armor_detecte
+    {
     public:
         armor_detecte();
         armor_detecte(const InputImg &);
@@ -22,35 +23,36 @@ namespace armor_detection{
         void setImg_origin(const InputImg &);
 
         void loadParam(bool enermy_color, bool isParam,
-                    int alpha = -1, int beta = -1, int thresh_value = -1,
+                       int alpha = -1, int beta = -1, int thresh_value = -1,
 
-                    double light_bit_min = -1, double light_bit_max = -1, double light_angle_min = -1, double light_angle_max = -1,
-                    double light_area_min = -1, double light_area_max = -1, double light_angle_abs = 4,
+                       double light_bit_min = -1, double light_bit_max = -1, double light_angle_min = -1, double light_angle_max = -1,
+                       double light_area_min = -1, double light_area_max = -1, double light_angle_abs = 4,
 
-                    double armor_bit_min = -1, double armor_bit_max = -1, double armor_area_min = -1, double armor_area_max = -1);
-        
-        // 得到与运算图像                
-        void init(); 
+                       double armor_bit_min = -1, double armor_bit_max = -1, double armor_area_min = -1, double armor_area_max = -1);
+
+        // 得到与运算图像
+        void init();
 
         void contours();
-        
+
         void contours_to_light();
 
         void light_to_armor();
 
         void drawrotate();
 
-        //tool
+        // tool
         void showImg();
         void drawContours();
         const static bool ENERMY_COLOR_BLUE = 1;
         const static bool ENERMY_COLOR_RED = 0;
+        const static double _PI_;
 
     private:
         cv::Mat img_origin, img_change, img_thresh, img_erode, img_dilate, img_and, img_combination;
 
         bool enermy_color;
-        
+
         // init
         int alpha;
         int beta;
@@ -71,13 +73,13 @@ namespace armor_detection{
         double armor_area_min;
         double armor_area_max;
 
-        //contours
+        // contours
         std::vector<cv::RotatedRect> contours_rects;
         std::vector<RotatedRect> light_rects;
         std::vector<RotatedRect> armor_rects;
-        
-        //tool
+
+        // tool
         void four_to_one(const std::vector<cv::Mat> &imgs, OutputImg &ans);
-        };
+    };
 }
 #endif // !_ARMOR_DETECTION_
